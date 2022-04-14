1ec56935897832cbf675aaae9708c1d4f5b6e820
MathxStudio <allan_pan_mathx123@163.com>
2022-04-14 20:50:25 +0800

    updates.

---
+++ Programming/diagram_for_boarding_types.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('./resultss.csv')
+print(df)
+
+
+# time_df = df.drop('dissatisfaction')
+# print(time_df)
